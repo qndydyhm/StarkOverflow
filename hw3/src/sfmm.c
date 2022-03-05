65b71e11e5db0b9ed9cc5cf3838b6ce476b06045
Gene Stark <gene@starkeffect.com>
2022-03-05 12:43:06 -0500

    Bring in basecode from the development repo.

---
+++ hw3/src/sfmm.c
@@ -0,0 +1,34 @@
+/**
+ * Do not submit your assignment with a main function in this file.
+ * If you submit with a main function in this file, you will get a zero.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "debug.h"
+#include "sfmm.h"
+
+void *sf_malloc(sf_size_t size) {
+    // TO BE IMPLEMENTED
+    abort();
+}
+
+void sf_free(void *pp) {
+    // TO BE IMPLEMENTED
+    abort();
+}
+
+void *sf_realloc(void *pp, sf_size_t rsize) {
+    // TO BE IMPLEMENTED
+    abort();
+}
+
+double sf_internal_fragmentation() {
+    // TO BE IMPLEMENTED
+    abort();
+}
+
+double sf_peak_utilization() {
+    // TO BE IMPLEMENTED
+    abort();
+}
